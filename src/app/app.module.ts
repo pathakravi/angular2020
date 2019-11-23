43103a9a3c01da133d79c98a035358dbe8b79922
pathakravi <ravipathak0804@gmail.com>
2019-11-24 02:21:27 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ 
     AppComponent,
-    ServerComponent
+    ServerComponent, ServersComponent
     ],
   bootstrap:    [ AppComponent ]
 })
